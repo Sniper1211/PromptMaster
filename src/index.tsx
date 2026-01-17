7b15150e28bfd26943fa6602ccf751e830238867
Sniper1211 <busivaa@gmail.com>
2026-01-17 21:02:19 +0800

    fix: add HelmetProvider to root to enable SEO meta tag injection

--- src/index.tsx
+++ src/index.tsx
@@ -1,6 +1,7 @@
 
 import React from 'react';
 import ReactDOM from 'react-dom/client';
+import { HelmetProvider } from 'react-helmet-async';
 import '@fontsource/inter/300.css';
 import '@fontsource/inter/400.css';
 import '@fontsource/inter/500.css';
@@ -18,6 +19,8 @@ if (!rootElement) {
 const root = ReactDOM.createRoot(rootElement);
 root.render(
   <React.StrictMode>
-    <App />
+    <HelmetProvider>
+      <App />
+    </HelmetProvider>
   </React.StrictMode>
 );
